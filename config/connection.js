e57c10cc7fc3da5ad6c8c29a1f7b2084708ceb84
John Calvin <ashannon1990@gmail.com>
2019-05-31 23:01:21 -0400

    changed to jawsdb database

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "****",
-  database: "burgers_db"
+  user: "ncyc0reyu2j5q6vq",
+  password: "g8kumct43n36bn4y",
+  database: "tybi410szf0bgs4p"
 });
 }
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,14 +9,12 @@ router.get("/", function(req, res) {
       var hbsObject = {
         burgers: data
       };
-      console.log(hbsObject);
       res.render("index", hbsObject);
     });
   });
 
   router.post("/api/burgers", function(req, res) {
     burger.create(
-        // took out values for "devoured", since by default it should be false
       "name",
       req.body.name, function(result) {
 
@@ -26,14 +24,10 @@ router.get("/", function(req, res) {
 
   router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
-  
-    console.log("condition", condition);
-  
     burger.update({
       devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
         res.status(200).end();
