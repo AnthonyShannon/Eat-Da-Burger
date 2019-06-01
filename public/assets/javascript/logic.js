a0ac952e78bba5d751edc15be9a210cfe246d3dc
John Calvin <ashannon1990@gmail.com>
2019-06-01 00:45:39 -0400

    tried some updates for heroku

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -18,38 +18,4 @@ $(function () {
     );
   });
 
-  $.ajax("/api/burgers/" + id, {
-    type: "DELETE",
-    success: function (result) {
-      console.log(result);
-    }
-  }).then(
-    function () {
-      location.reload();
-    }
-  );
-});
-
-$("#addBurger").on("submit", function (event) {
-
-  event.preventDefault();
-  if ($("#burger").val() !== "") {
-    var newBurger = {
-      name: $("#burgerToAdd").val().trim()
-    };
-
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function () {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  } else {
-    return false;
-  }
 });
