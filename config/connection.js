4a235c934b58f2077b7b733918b0e74b165f3638
John Calvin <ashannon1990@gmail.com>
2019-05-31 23:07:11 -0400

    tried some updates for heroku

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,19 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 var connection = mysql.createConnection({
-  host: "ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  host: "localhost",
   port: 3306,
-  user: "ncyc0reyu2j5q6vq",
-  password: "g8kumct43n36bn4y",
-  database: "tybi410szf0bgs4p"
+  user: "root",
+  password: "***",
+  database: "burgers_db"
 });
 }
+  // jawsdb info
+  // host: "ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  // port: 3306,
+  // user: "ncyc0reyu2j5q6vq",
+  // password: "g8kumct43n36bn4y",
+  // database: "tybi410szf0bgs4p"
 
 // Make connection.
 connection.connect(function(err) {
